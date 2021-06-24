3c997c659f557ddf84bc6b6970b0854ef067c1f2
tuankiet-kii <76982160+tuankiet-kii@users.noreply.github.com>
2021-06-25 00:06:50 +0700

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Lab2.Models
+{
+    public class Book
+    {
+        private int id;
+        private string title;
+        private string author;
+        private string image_cover;
+
+        public Book()
+        {
+        }
+
+        public Book(int id, string title, string author, string images)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.image_cover = images;
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        [Required(ErrorMessage = "Tiêu đề không được trống")]
+        [StringLength(250, ErrorMessage = "Tieu de khong duoc qua 250 ky tu")]
+        [Display(Name = "Tieu de")]
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ImageCover
+        {
+            get { return image_cover; }
+            set { image_cover = value; }
+        }      
+    }
+}
